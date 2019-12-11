1ac8191f5356a815b0ee5c01e68b4ee844b52ec9
lukassz111 <lukassz111@gmail.com>
2019-12-11 12:56:37 +0100

    taski

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,22 +2,66 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      exec: {
-        svg: {
-          cmd: 'xslt video_games_svg.xsl video_games_support.xml video_games.svg'
-        }
-      },
-      watch: {
-        svg: {
-          files: ['**/video_games_svg.xsl'],
-          tasks: ['exec:svg'],
-          options: {
-            spawn: false,
-            livereload: false
-          },
+        pkg: grunt.file.readJSON('package.json'),
+        exec: {
+            pdf: {
+                cmd: 'xsltfopdf video_games_pdf.xsl video_games_support.xml video_games.pdf'
+            },
+            svg: {
+                cmd: 'xslt video_games_svg.xsl video_games_support.xml video_games.svg'
+            },
+            txt: {
+                cmd: 'xslt video_games_txt.xsl video_games_support.xml video_games.txt'
+            },
+            html: {
+                cmd: 'xslt video_games_html.xsl video_games_support.xml video_games.html'
+            },
+            support: {
+                cmd: 'xslt video_games_support.xsl video_games.xml video_games_support.xml'
+            }
         },
-      },
+        watch: {
+            pdf: {
+                files: ['**/video_games_pdf.xsl'],
+                tasks: ['exec:pdf'],
+                options: {
+                    spawn: false,
+                    livereload: false
+                },
+            },
+            svg: {
+                files: ['**/video_games_svg.xsl'],
+                tasks: ['exec:svg'],
+                options: {
+                    spawn: false,
+                    livereload: false
+                },
+            },
+            txt: {
+                files: ['**/video_games_txt.xsl'],
+                tasks: ['exec:txt'],
+                options: {
+                    spawn: false,
+                    livereload: false
+                },
+            },
+            html: {
+                files: ['**/video_games_html.xsl'],
+                tasks: ['exec:html'],
+                options: {
+                    spawn: false,
+                    livereload: false
+                },
+            },
+            support: {
+                files: ['**/video_games_support.xsl'],
+                tasks: ['exec:support'],
+                options: {
+                    spawn: false,
+                    livereload: false
+                },
+            },
+        },
     });
   
     // Load the plugin that provides the "uglify" task.
