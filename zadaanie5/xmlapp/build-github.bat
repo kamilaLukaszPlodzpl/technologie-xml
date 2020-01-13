62865cf8e101354e6e6dca57d00088be61667c04
lukassz111 <lukassz111@gmail.com>
2020-01-13 01:45:41 +0100

    poprawione

--- zadaanie5/xmlapp/build-github.bat
+++ zadaanie5/xmlapp/build-github.bat
@@ -1,2 +1,1 @@
-del /F /S dist
 ng build --prod --deploy-url=https://kamilalukaszplodzpl.github.io/technologie-xml/z5/ --base-href=/technologie-xml/z5/
--- zadaanie5/xmlapp/build-golive.bat
+++ zadaanie5/xmlapp/build-golive.bat
@@ -1,2 +1,1 @@
-del /F /S dist
 ng build --prod --deploy-url=http://127.0.0.1:5500/dist/xmlapp/ --base-href=/dist/xmlapp/
